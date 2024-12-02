aadc2d627d7a59622656652a6afbf60dea264baf
titan\israel <blas@irakasle.eus>
2024-12-02 11:34:53 +0100

    FIX añadidas requires a las partes que faltaban por exponer

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,10 @@ module es.israeldelamo.jasperconmaven {
     requires javafx.fxml;
     requires java.sql;
     requires net.sf.jasperreports.core;
+    requires org.mariadb.jdbc;
 
+requires slf4j.api;
+requires slf4j.simple;
 
     opens es.israeldelamo.jasperconmaven to javafx.fxml, javafx.base;
     exports es.israeldelamo.jasperconmaven to javafx.graphics;
